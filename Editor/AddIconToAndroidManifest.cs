0b2180bedf70e78ec6132913437a86b40b4da0ba
dat-dangba <dat.dangba@tapbi.vn>
2026-01-14 14:56:38 +0700

    update namespace

--- Editor/AddIconToAndroidManifest.cs
+++ Editor/AddIconToAndroidManifest.cs
@@ -4,30 +4,33 @@ using System.Xml;
 using UnityEditor;
 using UnityEditor.Callbacks;
 
-public static class AndroidManifestMerger
+namespace DBD.Notification.Editor
 {
-    private const string ANDROID_NS = "http://schemas.android.com/apk/res/android";
-
-    [PostProcessBuild]
-    public static void OnPostprocessBuild(BuildTarget buildTarget, string pathToBuiltProject)
+    public static class AndroidManifestMerger
     {
-        if (buildTarget != BuildTarget.Android)
-            return;
+        private const string ANDROID_NS = "http://schemas.android.com/apk/res/android";
 
-        string manifestPath = Path.Combine(pathToBuiltProject, "launcher/src/main/AndroidManifest.xml");
+        [PostProcessBuild]
+        public static void OnPostprocessBuild(BuildTarget buildTarget, string pathToBuiltProject)
+        {
+            if (buildTarget != BuildTarget.Android)
+                return;
 
-        var xml = new XmlDocument();
-        xml.Load(manifestPath);
+            string manifestPath = Path.Combine(pathToBuiltProject, "launcher/src/main/AndroidManifest.xml");
 
-        var manifest = xml.SelectSingleNode("/manifest");
-        var application = manifest.SelectSingleNode("application");
+            var xml = new XmlDocument();
+            xml.Load(manifestPath);
 
-        var meta = xml.CreateElement("meta-data");
-        meta.SetAttribute("name", ANDROID_NS, "com.google.firebase.messaging.default_notification_icon");
-        meta.SetAttribute("resource", ANDROID_NS, "@drawable/icon_small_notification");
-        application.AppendChild(meta);
+            var manifest = xml.SelectSingleNode("/manifest");
+            var application = manifest.SelectSingleNode("application");
 
-        xml.Save(manifestPath);
+            var meta = xml.CreateElement("meta-data");
+            meta.SetAttribute("name", ANDROID_NS, "com.google.firebase.messaging.default_notification_icon");
+            meta.SetAttribute("resource", ANDROID_NS, "@drawable/icon_small_notification");
+            application.AppendChild(meta);
+
+            xml.Save(manifestPath);
+        }
     }
 }
 #endif
